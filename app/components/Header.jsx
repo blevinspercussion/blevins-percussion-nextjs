d1e828dc4a9d48a66d4f1f027f552a475cc21c27
Adam Blevins <blevinspercussion@gmail.com>
2023-06-14 14:03:48 -0500

    Can now show/hide mobile menu

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,49 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  const showMobileMenu = () => {
+    setMobileMenuVisible(true);
+    console.log("clicked");
+  };
+
+  const hideMobileMenu = () => {
+    setMobileMenuVisible(false);
+  };
+
   return (
     <div id="header">
+      {mobileMenuVisible && (
+        <div className="mobile-menu">
+          <Image
+            src="/../public/icons/exit-icon.png"
+            width={25}
+            height={25}
+            className="icon-exit"
+            onClick={hideMobileMenu}
+            alt="exit button"
+          />
+          <ul>
+            <li>
+              <Link href="/performance">Performance</Link>
+            </li>
+            <li>
+              <Link href="/instruction">Instruction</Link>
+            </li>
+            <li>
+              <Link href="/media">Media</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      )}
       <Image
         src="/../public/photos/blevperclogo_white_cropped.png"
         width="150"
@@ -14,6 +55,7 @@ const Header = () => {
         width="40"
         height="40"
         className="mobile-menu-icon"
+        onClick={showMobileMenu}
       />
     </div>
   );
