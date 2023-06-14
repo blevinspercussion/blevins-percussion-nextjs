388b0f1df0a71c8ea0cb02dfc3a7b64982a84d59
Adam Blevins <blevinspercussion@gmail.com>
2023-06-14 11:16:59 -0400

    basic layout for hero is in place

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,15 +5,15 @@ const Header = () => {
     <div id="header">
       <Image
         src="/../public/photos/blevperclogo_white_cropped.png"
-        width="195"
-        height="90"
+        width="150"
+        height="65"
         className="logo"
       />
       <Image
         src="/../public/icons/icons8-menu-rounded-30.png"
-        width="50"
-        height="50"
-        className="mobile-menu"
+        width="40"
+        height="40"
+        className="mobile-menu-icon"
       />
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -4,18 +4,33 @@ import Header from "./components/Header";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="main-content">
       <Header />
       <hr />
-      <div className="hero">
-        <Image
+      <div>
+        {/* <Image
           src="/../public/photos/green_drumlites.jpg"
           fill={false}
           height={2500}
           width={2500}
           alt="adam blevins playing drums"
           className="hero-image"
+        /> */}
+        <Image
+          src="/../public/photos/banner.jpg"
+          height={1000}
+          width={500}
+          className="hero-image"
+          alt="adam blevins playing drums"
         />
+      </div>
+      <div className="hero">
+        <h1>Adam Blevins, Drummer/Percussionist</h1>
+        <button className="btn-cta">Contact Me!</button>
+        <h3>Expert drums, percussion, and education</h3>
+        <h3>
+          Serving Chattanooga, North Georgia and the Southeastern United States
+        </h3>
       </div>
     </div>
   );
