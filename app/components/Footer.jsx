ea6c8cfc6770094119717bdd5b0e69bf91230f01
Adam Blevins <blevinspercussion@gmail.com>
2023-06-14 14:26:09 -0500

    added content to Footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      Site design copyright &copy 2023{" "}
+      <Link href="https://adamb.codes">adamb.codes</Link>
+    </div>
+  );
 };
 
 export default Footer;
